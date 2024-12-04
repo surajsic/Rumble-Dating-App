1c56c768a8696df46f53c09b06837b0e55e748f8
surajsic28@gmail.com <142887205+surajsic@users.noreply.github.com>
2024-12-04 23:31:08 +0530

    mongo

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const MONGO_URI= "mongodb://127.0.0.1:27017/match-up"
+//const MONGO_URI= "mongodb://127.0.0.1:27017/match-up"
 
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(MONGO_URI);
+		const conn = await mongoose.connect(process.env.MONGO_URI);
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.log("Error connecting to MongoDB: ", error);
