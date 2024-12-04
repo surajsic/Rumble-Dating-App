48baed83a1fb46b8d505c9440e3b2cad4774009f
surajsic28@gmail.com <142887205+surajsic@users.noreply.github.com>
2024-12-04 22:40:38 +0530

    json changes

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use('/api/messages', messageRoutes);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-	app.get("*", (req,res)=>{
+	app.get("*", (_,res)=>{
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")) 
 	});
 }
